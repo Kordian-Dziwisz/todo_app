721822dc498694897613f64b188c49fd7b8e3521
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-09-30 12:09:42 +0200

    removed adding object to firebase in main.js

--- src/main.js
+++ src/main.js
@@ -12,14 +12,6 @@ import firebaseConfig from './firebase'
 
 firebase.initializeApp(firebaseConfig)
 const firestore = firebase.firestore()
-firestore
-	.collection('users')
-	.add({
-		config: { darkMode: false },
-		name: 'Kordian'
-	})
-	.then(console.log('transaction completed'))
-	.catch(console.log('transaction not completed'))
 
 new Vue({
 	router,
