42277b3ccced25e55b11773bcb524d0f62b22340
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-09-30 12:07:45 +0200

    cleared router, views, added firebase config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
+//basic vue components
 import Vue from 'vue'
-import App from './App.vue'
+Vue.config.productionTip = false
+import App from './app.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+//firebase init
+import firebase from 'firebase'
+require('firebase/firestore')
+import firebaseConfig from './firebase'
+
+firebase.initializeApp(firebaseConfig)
+const firestore = firebase.firestore()
+firestore
+	.collection('users')
+	.add({
+		config: { darkMode: false },
+		name: 'Kordian'
+	})
+	.then(console.log('transaction completed'))
+	.catch(console.log('transaction not completed'))
 
 new Vue({
 	router,
