d45eeb6f3c73fcd4d5d7933b5ee731a20e5b10df
Michał Ronkowski <michalronkowski@gmail.com>
2019-10-02 12:03:27 +0200

    Dodanie komponentu side-bara

--- src/router.js
+++ src/router.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/login'
 import Register from './views/register'
+import UserPanel from './views/user-panel'
 
 Vue.use(Router)
 
 export default new Router({
 	mode: 'history',
 	base: process.env.BASE_URL,
-	routes: [	
+	routes: [
 		{ path: '/login', component: Login },
-		{ path: '/register', component: Register}
+		{ path: '/register', component: Register },
+		{ path: '/user-panel', component: UserPanel }
 	]
 })
---
+++ src/views/user-panel.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<side-bar></side-bar>
+	</div>
+</template>
+
+<script>
+import SideBar from '../components/side-bar'
+export default {
+	components: {
+		SideBar
+	}
+}
+</script>
