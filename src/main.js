2998a5a84bfd38533ae876d230cc7bb4594d66ae
hubertkowalski <kowalski.hubercik1@gmail.com>
2019-10-02 20:40:20 +0200

    Stworzono poczatkowy wyglad NavBara. Dodano Fontawesome.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import router from './router'
 import store from './store'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+//library for fontawesome
+library.add(faUserSecret)
+//component for fontawesome
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 //firebase init
 import firebase from 'firebase/app'
