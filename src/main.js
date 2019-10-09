8de8f2db07c5a3e81a95d82578f0bd4a437518bd
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-10-09 12:57:31 +0200

    merge

--- src/main.js
+++ src/main.js
@@ -4,10 +4,20 @@ Vue.config.productionTip = false
 import App from './app.vue'
 import router from './router'
 import store from './store'
+
 //module imports
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import './firebase.config'
+
+//fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+//library for fontawesome
+library.add(faUserSecret)
+//component for fontawesome
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
 	router,
