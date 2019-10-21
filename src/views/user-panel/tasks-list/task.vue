94703b44cae0e0522512bedf14a6906c232a4cb1
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-10-21 13:11:18 +0200

    Added task component, changed task name to newTask in tasks-list.vue

---
+++ src/views/user-panel/tasks-list/task.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		{{task.id}}
+		{{task.title}}
+		{{task.content}}
+		<br />
+	</div>
+</template>
+<script>
+export default {
+	props: {
+		task: undefined
+	}
+}
+</script>
--- src/views/user-panel/tasks-list/tasks-list.vue
+++ src/views/user-panel/tasks-list/tasks-list.vue
@@ -1,21 +1,25 @@
 <template>
-		<div class="card h-50 col-5">
-			<div class="card-body">
-				<h5 class="card-title">Tytuł</h5>
-				<h6 class="card-subtitle mb-2 text-muted">Podtytuł</h6>
-				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie pretium pellentesque. Praesent elit dui, tempor non imperdiet sed, egestas eget felis. Quisque quis elit nunc. Aenean erat quam, aliquam vel nunc eu, pulvinar laoreet purus. Curabitur non eros nisl. Curabitur viverra commodo dolor fermentum dignissim. Maecenas id vulputate tellus. Nulla volutpat imperdiet rutrum. Proin fermentum diam ut odio semper gravida et in erat. In quis odio sapien. Ut finibus elit a euismod placerat. Aenean felis diam, congue sed neque sed, blandit suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-			</div>
+	<div class="card h-50 col-5">
+		<div class="card-body">
+			<h5 class="card-title">Tytuł</h5>
+			<h6 class="card-subtitle mb-2 text-muted">Podtytuł</h6>
+			<p
+				class="card-text"
+			>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie pretium pellentesque. Praesent elit dui, tempor non imperdiet sed, egestas eget felis. Quisque quis elit nunc. Aenean erat quam, aliquam vel nunc eu, pulvinar laoreet purus. Curabitur non eros nisl. Curabitur viverra commodo dolor fermentum dignissim. Maecenas id vulputate tellus. Nulla volutpat imperdiet rutrum. Proin fermentum diam ut odio semper gravida et in erat. In quis odio sapien. Ut finibus elit a euismod placerat. Aenean felis diam, congue sed neque sed, blandit suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
 		</div>
+	</div>
 </template>
 <script>
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import Task from './task'
 export default {
+	components: { Task },
 	data() {
 		return {
 			list: [],
-			task: {
+			newTask: {
 				title: '',
 				content: ''
 			}
@@ -37,7 +41,7 @@ export default {
 				})
 		},
 		mapList(task) {
-			return task.data()
+			return { ...task.data(), id: task.id }
 		},
 		addTask() {
 			firebase
@@ -45,12 +49,11 @@ export default {
 				.collection('projects')
 				.doc('A1X83DUed6fOPqwt1iJi')
 				.collection('tasks')
-				.add(this.task)
+				.add(this.newTask)
 				.catch(console.log)
 		}
 	}
 }
 </script>
 <style lang="scss" scoped>
-
 </style>
