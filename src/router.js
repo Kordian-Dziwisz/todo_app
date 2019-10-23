91f251ba699dbe1b58b40dc0976075c40609727d
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-10-23 14:31:40 +0200

    added redirection from / to /user-panel

--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ export default new Router({
 	mode: 'history',
 	base: process.env.BASE_URL,
 	routes: [
+		{ path: '/', redirect: '/user-panel' },
 		{ path: '/login', name: 'login', component: Login },
 		{ path: '/register', name: 'register', component: Register },
 		{ path: '/user-panel', name: 'user-panel', component: UserPanel }
