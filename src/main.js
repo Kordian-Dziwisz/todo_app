3614b645215a7056f61a7cea04aa650bf62c5bab
hubertkowalski <kowalski.hubercik1@gmail.com>
2019-10-24 21:07:10 +0200

    Dodanie ponownie ikon fontawesome bo sie usunęly przy pushu

--- src/main.js
+++ src/main.js
@@ -1,34 +1,35 @@
 //basic modules
 import Vue from 'vue'
-Vue.config.productionTip = false
 import App from './app.vue'
 import router from './router'
 import store from './store'
-
 //firebase
 import './firebase.config'
+//vendor
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+//fontawesome
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+    faArchive,
+    faCalendarAlt,
+    faCog,
+    faEllipsisV,
+    faLayerGroup,
+    faPlus,
+    faStar,
+    faTrashAlt,
+    faUser,
+} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+Vue.config.productionTip = false
 
 //jquery
 window.$ = require('jquery')
 
-//vendor
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-
-//fontawesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-	faPlus,
-	faStar,
-	faCog,
-	faCalendarAlt,
-	faLayerGroup,
-	faArchive,
-	faUser
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 //library for fontawesome
-library.add(faUser, faStar, faCalendarAlt, faLayerGroup, faArchive, faCog, faPlus)
+library.add(faUser, faStar, faCalendarAlt, faLayerGroup, faArchive, faCog, faPlus, faEllipsisV, faTrashAlt)
 //component for fontawesome
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
