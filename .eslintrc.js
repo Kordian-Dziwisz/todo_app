528633e4e2fcb641d9e1d51a83bb0887e0565095
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-12-17 13:03:48 +0100

    modified eslist, now it shouldn't show prettier warings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
-	extends: ['@vue/typescript', 'plugin:vue/recomended'],
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ['plugin:vue/essential', '@vue/typescript'],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+	},
 	parserOptions: {
 		parser: '@typescript-eslint/parser'
 	}
